6836d52c63b48ab30409ff784d1e228a4fd8ddc5
UnknowMilitiant <j.umariy4238@gmail.com>
2024-12-11 16:05:29 +0500

    Some changes

--- apps/bots/models.py
+++ apps/bots/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from solo.models import SingletonModel
 
 
 # Create your models here.
-class TelegramBotConfiguration(models.Model):
-    bot_token = models.CharField(max_length=250)
-    secret_key = models.CharField(max_length=250)
-    webhook_url = models.URLField(max_length=250)
-    admin = models.IntegerField()
+class TelegramBotConfiguration(SingletonModel):
+    bot_token = models.CharField(max_length=250, default="token")
+    secret_key = models.CharField(max_length=250, default="secret_key")
+    webhook_url = models.URLField(max_length=250, default="https://api.telegram.org/")
+    admin = models.IntegerField(default=12345678)
--- apps/bots/views.py
+++ apps/bots/views.py
@@ -6,6 +6,8 @@ from django.utils.decorators import method_decorator
 
 # Ваш импорт для бота, если требуется
 from apps.bots.config.bot import dp
+from apps.bots.models import TelegramBotConfiguration
+from aiogram import Bot
 
 
 # Применяем декоратор @csrf_exempt к методу POST
@@ -15,6 +17,12 @@ class TelegramWebhook(View):
         return super().dispatch(*args, **kwargs)
 
     def post(self, request):
+
+        bot = Bot(
+            token=config.BOT_TOKEN,
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        )
+
         try:
             data = json.loads(request.body)
             print(data)
